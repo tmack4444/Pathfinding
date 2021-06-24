6b3250ec32c491da3651394e294e6d9965b0ece4
tmack4444 <>
2021-06-24 15:53:01 -0400

    Started working on the BFS implementation, going to start with reading the maze and simply outputting it to console, implementing the algo will come second

---
+++ BFS.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+/* @Author Tom Mackinson
+ * Implementation of Breadth first search for pathfinding
+ * Uses a standard set of mazes, stored in ./Mazes
+ *
+ * In a maze
+ * 0 is a valid space to move
+ * 1 is a wall, an invalid space
+ * 2 is the starting point for the maze
+ * 3 is the end point of the maze, the destination
+ */
+
+public class BFS{
+
+    // main loads the maze in as a 2D array of int
+    // It then calls the algorithm with the maze array
+    public static void main(String args[]){
+      Scanner input = new Scanner(System.in);
+
+      while(input.hasNextLine()){
+
+      }
+
+    }
+
+}
